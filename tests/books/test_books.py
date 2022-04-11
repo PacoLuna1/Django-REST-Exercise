04e24cd27bedb69f260f48443979f831a0ec5b29
PacoLuna1 <pakoo_15@hotmail.com>
2022-04-11 23:26:47 +0000

    Create test for publisher, location and genre

--- tests/books/test_books.py
+++ tests/books/test_books.py
@@ -2,10 +2,28 @@ import pytest
 from library.books.models import *
 
 @pytest.mark.django_db
-def test_author_name():
-  author = Author.objects.create(name='Paulo', last_name='Coelho') # Create a author
+@pytest.mark.parametrize(
+  'nombre, apellido',
+  (
+    ('Paulo', 'Coelho'),
+    ('Haruki', 'Murakami'),
+    ('Jordi', 'Rosado'),
+  )
+)
+def test_author_name(nombre, apellido):
+  author = Author.objects.create(name=nombre, last_name=apellido) # Create a author
   print('This is my authors name: ', author.name) # print author's name
-  assert author.last_name == 'Coelho' # validate last name of the author
+  assert author.last_name == apellido # validate last name of the author
   assert Author.objects.count() == 1
   author.delete()
   assert Author.objects.count() == 0
+  
+# @pytest.mark.django_db
+# def test_author_with_monkey(monkeypatch):
+#   author = Author.objects.create(name='nombre', last_name='apellido') # Create a author
+#   def model_count_mock():
+#     return 4
+  
+#   monkeypatch.setattr(Author.objects.all(), 'count', model_count_mock)
+#   assert Author.objects.all().count == 4
+#   print('Haciendo el monkeypatch')
